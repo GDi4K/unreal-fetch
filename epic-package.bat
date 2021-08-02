125c3bc263c97f8fc194d4cd9e23eb38bbac8a66
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2021-08-02 13:52:27 +0530

    1.1.2

--- epic-package.bat
+++ epic-package.bat
@@ -1,3 +1,3 @@
 echo "Epic Packaging ..."
 
-"D:\Programs\Epic Games\UE_4.25\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=D:\Repos\GameDev4K\unreal-fetch\UnrealFetchDevApp\Plugins\Fetch\Fetch.uplugin -Package=ToPackage -Rocket
+"D:\Programs\Epic Games\UE_4.26\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=D:\Repos\GameDev4K\unreal-fetch\UnrealFetchDevApp\Plugins\Fetch\Fetch.uplugin -Package=ToPackage -Rocket
