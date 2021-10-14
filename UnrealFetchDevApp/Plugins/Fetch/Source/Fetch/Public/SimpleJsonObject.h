5bb6d05eb8626bf0ce152ef4e9f72bc4f21d5906
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2021-10-14 14:54:59 +0530

    Introduce Sync Methods to support GA

--- UnrealFetchDevApp/Plugins/Fetch/Source/Fetch/Public/SimpleJsonObject.h
+++ UnrealFetchDevApp/Plugins/Fetch/Source/Fetch/Public/SimpleJsonObject.h
@@ -19,55 +19,140 @@ class FETCH_API USimpleJsonObject : public UObject
 
 public:
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		USimpleJsonObject* GetObject(FString Field);
+	USimpleJsonObject* GetObject(FString Field);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	USimpleJsonObject* GetObjectSync(FString Field)
+	{
+		return GetObject(Field);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		float GetNumber(FString Field);
+	float GetNumber(FString Field);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	float GetNumberSync(FString Field)
+	{
+		return GetNumber(Field);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		FString GetString(FString Field);
+	FString GetString(FString Field);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	FString GetStringSync(FString Field)
+	{
+		return GetString(Field);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		bool GetBool(FString Field);
+	bool GetBool(FString Field);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	bool GetBoolSync(FString Field)
+	{
+		return GetBool(Field);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		TArray<USimpleJsonValue*> GetArray(FString Field);
+	TArray<USimpleJsonValue*> GetArray(FString Field);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	TArray<USimpleJsonValue*> GetArraySync(FString Field)
+	{
+		return GetArray(Field);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		bool IsNull(FString Field);
+	bool IsNull(FString Field);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	bool IsNullSync(FString Field)
+	{
+		return IsNull(Field);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		bool IsNone(FString Field);
+	bool IsNone(FString Field);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	bool IsNoneSync(FString Field)
+	{
+		return IsNone(Field);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		bool IsEmpty(FString Field);
+	bool IsEmpty(FString Field);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	bool IsEmptySync(FString Field)
+	{
+		return IsEmpty(Field);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-        bool HasKey(FString Field);
+	bool HasKey(FString Field);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	bool HasKeySync(FString Field)
+	{
+		return HasKey(Field);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		USimpleJsonObject* SetNumber(FString Field, float Value);
+	USimpleJsonObject* SetNumber(FString Field, float Value);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	USimpleJsonObject* SetNumberSync(FString Field, float Value)
+	{
+		return SetNumber(Field, Value);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		USimpleJsonObject* SetString(FString Field, FString Value);
+	USimpleJsonObject* SetString(FString Field, FString Value);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	USimpleJsonObject* SetStringSync(FString Field, FString Value)
+	{
+		return SetString(Field, Value);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		USimpleJsonObject* SetBool(FString Field, bool Value);
+	USimpleJsonObject* SetBool(FString Field, bool Value);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	USimpleJsonObject* SetBoolSync(FString Field, bool Value)
+	{
+		return SetBool(Field, Value);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		USimpleJsonObject* SetObject(FString Field, USimpleJsonObject* JsonObjet);
+	USimpleJsonObject* SetObject(FString Field, USimpleJsonObject* JsonObjet);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	USimpleJsonObject* SetObjectSync(FString Field, USimpleJsonObject* JsonObjet)
+	{
+		return SetObject(Field, JsonObjet);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		USimpleJsonObject* SetArray(FString Field, TArray<USimpleJsonValue*> Value);
+	USimpleJsonObject* SetArray(FString Field, TArray<USimpleJsonValue*> Value);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	USimpleJsonObject* SetArraySync(FString Field, TArray<USimpleJsonValue*> Value)
+	{
+		return SetArray(Field, Value);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		FString Stringify(bool pretty = false);
+	FString Stringify(bool pretty = false);
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	FString StringifySync(bool pretty = false)
+	{
+		return Stringify(pretty);
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		USimpleJsonValue* AsValue();
+	USimpleJsonValue* AsValue();
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	USimpleJsonValue* AsValueSync()
+	{
+		return AsValue();
+	}
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Fetch | SimpleJsonObject")
-		bool IsValidObject();
+	bool IsValidObject();
+	UFUNCTION(BlueprintCallable, Category = "Fetch | SimpleJsonObject")
+	bool IsValidObjectSync()
+	{
+		return IsValidObject();
+	}
 
 	static USimpleJsonObject* Get(TSharedPtr<FJsonObject> Json);
 	static USimpleJsonObject* GetInvalid();
