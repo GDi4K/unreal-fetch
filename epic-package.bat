0289d10d03a4b5a5b89f293ad2f11e7d7b171d88
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2022-11-18 02:37:33 +0530

    Add support for 5.1

--- epic-package.bat
+++ epic-package.bat
@@ -1,3 +1,3 @@
 echo "Epic Packaging ..."
 
-"D:\Programs\Epic Games\UE_%1\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=D:\Repos\GDi4K\unreal-fetch\UnrealFetchDevApp\Plugins\Fetch\Fetch.uplugin -Package=ToPackage -Rocket
+"C:\Program Files\Epic Games\UE_%1\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=C:\Data\Repos\GDi4K\unreal-fetch\UnrealFetchDevApp\Plugins\Fetch\Fetch.uplugin -Package=ToPackage -Rocket
--- package.bat
+++ package.bat
@@ -1,18 +1,46 @@
 rmdir /S /Q ToPackage
 
 md ToPackage
-md ToPackage\Fetch
+md ToPackage\Fetch_46
 
-xcopy UnrealFetchDevApp\Plugins\Fetch\Fetch.uplugin ToPackage\Fetch
+xcopy UnrealFetchDevApp\Plugins\Fetch\Fetch.uplugin ToPackage\Fetch_46
 
-md ToPackage\Fetch\Config
-xcopy UnrealFetchDevApp\Plugins\Fetch\Config ToPackage\Fetch\Config /E/H
+md ToPackage\Fetch_46\Config
+xcopy UnrealFetchDevApp\Plugins\Fetch\Config ToPackage\Fetch_46\Config /E/H
 
-md ToPackage\Fetch\Resources
-xcopy UnrealFetchDevApp\Plugins\Fetch\Resources ToPackage\Fetch\Resources /E/H
+md ToPackage\Fetch_46\Resources
+xcopy UnrealFetchDevApp\Plugins\Fetch\Resources ToPackage\Fetch_46\Resources /E/H
 
-md ToPackage\Fetch\Source
-xcopy UnrealFetchDevApp\Plugins\Fetch\Source ToPackage\Fetch\Source /E/H
+md ToPackage\Fetch_46\Source
+xcopy UnrealFetchDevApp\Plugins\Fetch\Source ToPackage\Fetch_46\Source /E/H
 
-md ToPackage\Fetch\Content
-xcopy UnrealFetchDevApp\Plugins\Fetch\Content ToPackage\Fetch\Content /E/H
+md ToPackage\Fetch_46\Content
+xcopy UnrealFetchDevApp\Plugins\Fetch\Content ToPackage\Fetch_46\Content /E/H
+
+echo "Copying the 4.7 version"
+md ToPackage\Fetch_47
+xcopy ToPackage\Fetch_46 ToPackage\Fetch_47 /E/H
+
+cscript //NoLogo sed.vbs s/(4.26.0)/4.27.0/ < ToPackage\Fetch_46\Fetch.uplugin > ToPackage\Fetch_47\Fetch.uplugin
+
+echo "Copying the 5.0 version"
+md ToPackage\Fetch_50
+xcopy ToPackage\Fetch_46 ToPackage\Fetch_50 /E/H
+
+cscript //NoLogo sed.vbs s/(4.26.0)/5.0.0/ < ToPackage\Fetch_46\Fetch.uplugin > ToPackage\Fetch_50\Fetch.uplugin
+
+echo "Copying the 5.1 version"
+md ToPackage\Fetch_51
+xcopy ToPackage\Fetch_46 ToPackage\Fetch_51 /E/H
+
+cscript //NoLogo sed.vbs s/(4.26.0)/5.1.0/ < ToPackage\Fetch_46\Fetch.uplugin > ToPackage\Fetch_51\Fetch.uplugin
+
+@REM Identifying the version
+FOR /F "tokens=*" %%g IN ('git describe --tags') do (SET VERSION=%%g)
+
+@REM Making zip files
+cd ToPackage
+tar.exe -a -c -f Fetch_51_%VERSION%.zip Fetch_51
+tar.exe -a -c -f Fetch_50_%VERSION%.zip Fetch_50
+tar.exe -a -c -f Fetch_47_%VERSION%.zip Fetch_47
+tar.exe -a -c -f Fetch_46_%VERSION%.zip Fetch_46
